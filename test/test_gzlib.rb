6fd1e4f8bb043ce6c26abe71472ea87e15aeafdb
ken <blobk24block@gmail.com>
2016-03-06 22:49:43 +0800

    fetch until last page

--- test/test_gzlib.rb
+++ test/test_gzlib.rb
@@ -8,7 +8,7 @@ class TestGzlib < Minitest::Test
   end
 
   def test_has_ruby_book
-    assert_match /^ruby/i, @ruby.books.first.title
+    assert_match /^ruby/i, @ruby.first.title
   end
 
   def test_ruby_has_10_pages_more_result
@@ -16,6 +16,16 @@ class TestGzlib < Minitest::Test
   end
 
   def test_has_book_id
-    assert (not @ruby.books.first.id.nil?)
+    assert (not @ruby.first.id.nil?)
+  end
+
+  def test_can_fetch_book_in_next_page
+    assert @ruby.any?{|book| book.title.match /My true story/}
+    assert @ruby.curPage > 1
+  end
+
+  def test_can_fetch_books_until_last_page
+    assert @ruby.total > 100
+    assert @ruby.lastPage?
   end
 end
