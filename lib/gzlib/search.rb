16e8200a699c783d5794f8c2d5b14dfcc7b7c94b
ken <block24block@gmail.com>
2016-03-06 20:21:06 +0800

    get book id

--- lib/gzlib/search.rb
+++ lib/gzlib/search.rb
@@ -1,13 +1,13 @@
 class Search
-  @@url = 'http://opac.gzlib.gov.cn/opac/search?hasholding=1&searchWay=title&q='
+  Search = 'http://opac.gzlib.gov.cn/opac/search?hasholding=1&searchWay=title&q='
  
-  attr_reader :result, :pages
+  attr_reader :books, :pages
 
   def initialize(key)
-    doc = getHTML "#{@@url}#{escape key}"
-    @result = doc.css(".bookmeta").map do |node|
-                Book.new(node).title
-              end.to_a
+    doc = getHTML "#{Search}#{escape key}"
+    @books = doc.css(".bookmeta").map do |node|
+               Book.new(node)
+             end.to_a
     @pages = doc.at_css(".meneame .disabled").text.gsub(/[^\d]/,'').to_i
   end
 
