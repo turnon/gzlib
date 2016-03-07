a9c2c789b49b60ed7624bccb48e04af84621c97f
ken <block24block@gmail.com>
2016-03-08 00:16:08 +0800

    parse author, publisher, year

--- lib/gzlib/book.rb
+++ lib/gzlib/book.rb
@@ -2,11 +2,15 @@ class Book
 
   Search = "http://opac.gzlib.gov.cn/opac/book/holdingpreview/"
 
-  attr_reader :title, :id
+  attr_reader :title, :id, :author, :publisher, :year
 
   def initialize(node)
-    @title = node.at_css(".bookmetaTitle").text.gsub(/[\r\n\t]/,'')
+    @title = node.at_css(".bookmetaTitle").text.strip
     @id = node['bookrecno']
+    metas = node.css("div")
+    @author = metas[1].at_css("a").text.strip
+    @publisher = metas[2].at_css("a").text.strip
+    @year = metas[2].text.gsub(/[^\d]/,'')
   end
 
   def positions
