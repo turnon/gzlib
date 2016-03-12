39a9da9d7225cea6fa76bd2be10e36d5d62dbd3f
ken <block24block@gmail.com>
2016-03-12 20:57:08 +0800

    add test isbn

--- test/test_gzlib.rb
+++ test/test_gzlib.rb
@@ -6,6 +6,7 @@ class TestGzlib < Minitest::Test
   def setup
     @ruby = Gzlib.search 'ruby', rows: 10
     @score_ruby = Gzlib.search 'ruby', sortWay: 'score'
+    @matz_ruby = Gzlib.search '9787115366467', searchWay: 'isbn'
   end
 
   def test_has_ruby_book
@@ -28,5 +29,9 @@ class TestGzlib < Minitest::Test
   def test_can_fetch_books_until_last_page
     assert @ruby.total > 100
     assert @ruby.lastPage?
+  end
+
+  def test_one_isbn_one_book
+    assert @matz_ruby.map{|b| b.title}.uniq.size == 1
   end
 end
